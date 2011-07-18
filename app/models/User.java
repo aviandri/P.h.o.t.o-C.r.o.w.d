598ba1e36da0d3fda69003cfa8092f3832f63e65
uudashr <uudashr@gmail.com>
2011-07-19 00:41:53 +0700

    Remove the token pair since it deprecated

--- app/models/User.java
+++ app/models/User.java
@@ -1,13 +1,6 @@
 package models;
 
-import play.*;
-import play.libs.OAuth;
-import play.libs.OAuth.ServiceInfo;
-import play.libs.OAuth.TokenPair;
-
-import javax.mail.Session;
-import javax.persistence.*;
-import java.util.*;
+import javax.persistence.Entity;
 
 @Entity
 public class User extends Model {
@@ -15,11 +8,6 @@ public class User extends Model {
     public String secretToken;
     public String accessToken;
     public Long twitterId;
-    
-    public TokenPair getTokenPair(){
-    	TokenPair tokenPair = new TokenPair(accessToken, secretToken);
-    	return tokenPair;
-    }
     
     public static User findByUsername(String userName){
     	return User.find("byUsername", userName).first();
