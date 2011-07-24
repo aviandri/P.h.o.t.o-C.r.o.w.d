a4bc2e7b25c5c46b5ee870e59d432b63a3b9b09a
uudashr <uudashr@gmail.com>
2011-07-25 01:20:06 +0700

    Create dummy user galleries

---
+++ app/controllers/Users.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * @author uudashr@gmail.com
+ *
+ */
+public class Users extends Controller {
+    public static void galleries() {
+        render();
+    }
+}
