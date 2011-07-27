e8c048548efe62a0be74f8bfee85a545c3035a30
uudashr <uudashr@gmail.com>
2011-07-28 01:08:05 +0700

    Save the gallery to database

--- app/controllers/Galleries.java
+++ app/controllers/Galleries.java
@@ -2,6 +2,8 @@ package controllers;
 
 import java.util.Date;
 
+import controllers.Secure.Security;
+
 import models.Gallery;
 import play.data.binding.As;
 import play.data.validation.Match;
@@ -11,42 +13,49 @@ import play.mvc.With;
 
 /**
  * @author uudashr@gmail.com
- *
+ * 
  */
 @With(Secure.class)
 public class Galleries extends Controller {
-    
+
     public static void create() {
         renderCreate(null);
     }
-    
+
     private static void renderCreate(Gallery gallery) {
         render("@create", gallery);
     }
-    
-    public static void save(@Required(message="validation.required.gallery.name") String name, 
-            @As("MM/dd/yyyy") Date startDate, 
+
+    public static void save(
+            @Required(message = "validation.required.gallery.name") String name,
+            @As("MM/dd/yyyy") Date startDate,
             @As("MM/dd/yyyy") Date endDate,
-            @Required(message="validation.required.gallery.hashtag") @Match(value="#?([A-Za-z0-9_]+) *", message="validation.hashtag") String hashtag, 
-            String location, 
-            String description) {
-        
+            @Required(message = "validation.required.gallery.hashtag") @Match(value = "#?([A-Za-z0-9_]+) *", message = "validation.hashtag") String hashtag,
+            String location, String description) {
+
         if (endDate != null && startDate == null) {
-            validation.addError("endDate", "validation.required.reference.startDate");
+            validation.addError("endDate",
+                    "validation.required.reference.startDate");
             endDate = null;
         }
-        
+
         if (startDate != null && endDate != null) {
             validation.future(endDate, startDate);
         }
-        
+
         if (hashtag != null) {
             hashtag = hashtag.trim();
         }
-        
+
+        Gallery gallery = new Gallery(name, startDate, endDate, hashtag,
+                location, description);
         if (validation.hasErrors()) {
-            renderCreate(new Gallery(name, startDate, endDate, hashtag, location, description));
+            renderCreate(gallery);
         }
+
+        gallery.user = Security.connectedUser();
+        gallery.save();
+        
         renderText("OK");
     }
 }
--- app/models/Gallery.java
+++ app/models/Gallery.java
@@ -38,6 +38,7 @@ public class Gallery extends Model {
     
     public Boolean state = true;
     
+    @Required
     @ManyToOne
     public User user;
     
