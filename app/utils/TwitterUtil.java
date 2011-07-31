81899aceb18a00f65b09bae58ead959ded195af3
uudashr <uudashr@gmail.com>
2011-07-31 22:13:31 +0700

    Introduce the SearchResponse... is the class that wrap JsonObject of Twitter Search Response

--- app/utils/TwitterUtil.java
+++ app/utils/TwitterUtil.java
@@ -5,21 +5,20 @@ import play.libs.WS;
 import play.libs.WS.HttpResponse;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class TwitterUtil {
 
-    public static JsonArray searchTwitter(String q, Long sinceId) {
+    public static SearchResponse searchTwitter(String q, Long sinceId) {
         String callUrl = "http://search.twitter.com/search.json?q=" + q
                 + "&since_id=" + sinceId + "&rpp=100";
         Logger.info("Call twitter url:" + callUrl);
         HttpResponse resp = WS.url(callUrl).get();
-        JsonElement element = resp.getJson();
+        JsonObject jsonObj = resp.getJson().getAsJsonObject();
         if (resp.getStatus() != 200) {
-            String errorMesssage = element.getAsJsonObject().get("error").getAsString();
+            String errorMesssage = jsonObj.get("error").getAsString();
             Logger.error("Got non 200 HTTP Status code: %1s error=%2s", resp.getStatus(), errorMesssage);
         }
-        JsonArray results = element.getAsJsonObject().getAsJsonArray("results");
-        return results;
+        return new SearchResponse(jsonObj);
     }
 }
