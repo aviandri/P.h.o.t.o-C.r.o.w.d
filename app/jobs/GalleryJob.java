809460b2aac7fe160eec3be4042a2adccf7ace69
uudashr <uudashr@gmail.com>
2011-08-04 15:08:33 +0700

    parse date twitter and filter only until specified date

--- app/jobs/GalleryJob.java
+++ app/jobs/GalleryJob.java
@@ -1,8 +1,12 @@
 package jobs;
 
 import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import models.Gallery;
 import models.Gallery.State;
@@ -102,11 +106,28 @@ public class GalleryJob extends Job<Void> {
     
     private void processTweet(JsonElement tweet) {
         JsonObject tweetObject = tweet.getAsJsonObject();
-        String createdDateStr = tweetObject.getAsJsonPrimitive("created_at")
-                .getAsString();
+        
+        long id = tweetObject.getAsJsonPrimitive("id").getAsLong();
         String tweetText = tweetObject.getAsJsonPrimitive("text").getAsString();
         String username = tweetObject.getAsJsonPrimitive("from_user")
                 .getAsString();
+        String createdDateStr = tweetObject.getAsJsonPrimitive("created_at")
+                .getAsString();
+        
+        
+        DateFormat dateFormat = newDateFormat();
+        Date createdDate = null;
+        try {
+            createdDate = dateFormat.parse(createdDateStr);
+        } catch (ParseException e) {
+            Logger.error("Invalid date format of created_date %1s", createdDateStr);
+            return;
+        }
+        
+        if (gallery.endDate != null && createdDate.after(gallery.endDate)) {
+            Logger.debug("The tweet (%1s) date pass the end date.. skip", id);
+            return;
+        }
         
         Logger.debug("tweet text:" + tweetText);
         String[] urls = StringUtils.grabImageServiceURLs(tweetText);
@@ -118,6 +139,14 @@ public class GalleryJob extends Job<Void> {
     private void initPhotoJob(String tweetText, String username, String url) {
         GrabPhotoJob photoJob = new GrabPhotoJob(gallery, url, username, tweetText);
         photoJob.now();
+    }
+    
+    private static DateFormat newDateFormat() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "EEE, dd MMM yyyy HH:mm:ss ZZZZZZ", Locale.US);
+        dateFormat.setLenient(false);
+        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return dateFormat;
     }
 
 }
