7f50c8a93abfa2a70f0948291f717571db1c703a
uudashr <uudashr@gmail.com>
2011-08-08 12:31:52 +0700

    Remove the un-necessary test

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,8 +1,6 @@
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
-import java.util.TimeZone;
 
 import org.junit.Test;
 
@@ -15,14 +13,4 @@ public class BasicTest extends UnitTest {
         assertEquals(2, 1 + 1);
     }
     
-    @Test
-    public void dateTest() throws Exception {
-        String twitterDate = "Wed, 03 Aug 2011 18:24:47 +0000";
-        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
-        dateFormat.setLenient(false);
-        Date date = dateFormat.parse(twitterDate);
-        
-        assertEquals(twitterDate, dateFormat.format(date));
-    }
-
 }
